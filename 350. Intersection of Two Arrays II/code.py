230f180097b042de6bfcfa09c317c90054270a18
Arhaan <ironarhaan@gmail.com>
2024-07-02 19:14:31 +0530

    Adding codes

--- 350. Intersection of Two Arrays II/code.py
+++ 350. Intersection of Two Arrays II/code.py
@@ -1,8 +1,18 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res=[]
+        map1 = {}
+        
         for i in nums1:
-            if i in nums2:
-                res.append(i)
-                nums2.remove(i)
-        return res
+            if i in map1:
+                map1[i] += 1
+            else:
+                map1[i] = 1
+        
+        result = []
+
+        for i in nums2:
+            if i in map1 and map1[i] > 0:
+                result.append(i)
+                map1[i] -= 1
+        
+        return result
