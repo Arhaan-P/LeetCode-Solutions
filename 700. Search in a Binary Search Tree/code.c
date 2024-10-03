00bb8b7ce6e7a25be5886f83a30b0e19302e742d
Arhaan-P <ironarhaan@gmail.com>
2024-10-03 09:58:53 +0530

    Adding codes

---
+++ 700. Search in a Binary Search Tree/code.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (root == NULL) {
+        return NULL;
+    }
+    if (val < root->val) {
+        return searchBST(root->left, val);
+    }
+    else if (val > root->val) {
+         return searchBST(root->right, val);
+    }
+    else {
+        return root;
+    }
+}
