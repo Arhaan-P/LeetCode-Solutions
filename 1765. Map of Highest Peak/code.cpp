bcff6e729f09532814f312a7577be6ebdb587446
Arhaan-P <ironarhaan@gmail.com>
2025-01-22 18:39:01 +0530

    Adding codes

--- 1765. Map of Highest Peak/code.cpp
+++ 1765. Map of Highest Peak/code.cpp
@@ -1,42 +1,47 @@
 class Solution {
-public:
-
-    bool isValid (int x, int y, int n, int m) {
+    bool isValid(int x, int y, int m, int n) {
         return (x >= 0 && x < m && y >= 0 && y < n);
     }
 
+public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        queue<pair<int, int>> q;
         int m = isWater.size();
         int n = isWater[0].size();
+        vector<vector<int>> height(m, vector<int>(n, 0));
 
-        vector<vector<int>> height(m, vector<int>(n, -1));
-        queue<pair<int, int>> q;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (isWater[i][j] == 1) {
-                    height[i][j] = 0;
-                    q.push({i, j});
+                    q.push(make_pair(i, j));
+                    visited[i][j] = true;
                 }
             }
         }
 
         vector<int> dir = {-1, 0, 1, 0, -1};
-
+        int level = 0;
         while (!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                pair<int, int> curr = q.front();
+                q.pop();
 
-            for (int i = 0; i < 4; i++) {
-                int X = x + dir[i];
-                int Y = y + dir[i + 1];
-
-                if (isValid(X, Y, m, n) && height[X][Y] == -1) {
-                    height[X][Y] = height[x][y] + 1;
-                    q.push({X, Y});
+                int x = curr.first;
+                int y = curr.second;
+                for (int d = 0; d < 4; d++) {
+                    int X = x + dir[d];
+                    int Y = y + dir[d + 1];
+                    if (isValid(X, Y, m, n) && !visited[X][Y]) {
+                        q.push(make_pair(X, Y));
+                        height[X][Y] = level + 1;
+                        visited[X][Y] = true;
+                    }
                 }
             }
+            level++;
         }
-    return height;
+        return height;
     }
 };
