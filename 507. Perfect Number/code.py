506ca745162c49903ad8e479a063899ae5281317
Arhaan-P <ironarhaan@gmail.com>
2025-05-27 11:17:02 +0530

    Adding codes

--- 507. Perfect Number/code.py
+++ 507. Perfect Number/code.py
@@ -1,6 +1,8 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         divisor_sum = 1
+        if num <= 1:
+            return False
         for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 divisor_sum += i
